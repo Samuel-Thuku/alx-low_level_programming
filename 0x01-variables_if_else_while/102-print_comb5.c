a942fa1acae580d4784e1c7d1282f409b5c6d6dc
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-19 08:16:00 +0000

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit1, digit2, digit3, digit4;
+
+	digit1 = 0;
+	while (digit1 < 10)
+	{
+	digit2 = 0;
+	while (digit2 < 10)
+		{
+		digit3 = 1;
+		while (digit3 < 10)
+		{
+			digit4 = 2;
+			while (digit4 < 10)
+			{
+				putchar('0' + digit1);
+				putchar('0' + digit2);
+				putchar(' ');
+				putchar('0' + digit3);
+				putchar('0' + digit4);
+				if 
+				putchar(',');
+				putchar(' ');
+				digit4++;
+			}
+		digit3++;
+		}
+		digit2++;
+	}
+	digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
