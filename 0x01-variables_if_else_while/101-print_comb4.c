30dc54057898faf92692de5aafcf59d67f22f34b
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-19 06:58:22 +0000

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,34 @@
 */
 int main(void)
 {
-int digit1, digit2, digit3;
+	int digit1, digit2, digit3;
 
-digit1 = 0;
-while (digit1 < 10)
-{
-digit2 = 0;
-while (digit2 < 10)
-{
-digit3 = 0;
-while (digit3 < 10)
-{
-if (digit1 != digit2 && digit1 != digit3 && digit2 != digit3)
-{
-printf("%d%d%d ", digit1, digit2, digit3);
-}
-digit3++;
-}
-digit2++;
-}
-digit1++;
-}
-
-
-return (0);
+	digit1 = 0;
+	while (digit1 < 10)
+	{
+	digit2 = 0;
+	while (digit2 < 10)
+	{
+	digit3 = 0;
+	while (digit3 < 10)
+	{
+	if (digit3 > digit2 && digit2 > digit1)
+	{
+	putchar('0' + digit1);
+	putchar('0' + digit2);
+	putchar('0' + digit3);
+	if (digit1 < 7)
+	{
+	putchar(',');
+	putchar(' ');
 	}
+	}
+	digit3++;
+	}
+	digit2++;
+	}
+	digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
