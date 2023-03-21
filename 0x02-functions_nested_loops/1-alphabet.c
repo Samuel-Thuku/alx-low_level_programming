797a9e0b00242bf016cd36dee9c95509f05a5ab1
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 15:14:59 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet;
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
