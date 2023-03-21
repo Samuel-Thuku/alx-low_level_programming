c148fbcaa5710ccce0690b2b3feb75f2eeb88d78
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 16:28:30 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - determines lowercase characters
+ * @c: the letter being checked
+ *
+ * Return: 1 for lowercase characters 0 for anything else
+ */
+int _islower(int c)
+{
+
+	c = 97;
+	while (c <= 122)
+	{
+		return (1);
+		c++;
+	}
+	return (0);
+}
