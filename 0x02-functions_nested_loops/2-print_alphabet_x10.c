bca31dc62e2d559fd8b86e0f0377d95dc2eb0ac2
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 16:01:29 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Entry point
+*
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		c = 'a'
+			while (c <= 'z')
+			{
+				_putchar(c);
+				c++;
+			}
+		_putchar('\n');
+		num++;
+	}
+}
