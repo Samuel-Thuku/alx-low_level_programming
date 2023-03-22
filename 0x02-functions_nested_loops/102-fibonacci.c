ce3f8f28c31ef5abc8db78221c6ea950b1d95fca
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-22 21:50:02 +0000

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num1;
+	long int num2, num3, num4;
+
+	num2 = 0;
+	num3 = 1;
+	for (num1 = 0; num1 < 50; num1++)
+	{
+		num4 = num2 + num3;
+		printf("%ld, ", num4);
+		num2 = num3;
+		num3 = num4;
+	}
+	return (0);
+}
