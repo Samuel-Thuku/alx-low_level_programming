484e477184dee2449a1b9678024938e2867660a2
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 19:02:42 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add - gets the sum of wo numbers
+*@num1: one of the numbers to be added
+*@num2: the other number to be added
+*Return: sum the result of he addition of the two numbers
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
