433761a3fae03354d720f6ce76588bf7c0fa1f77
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 17:25:19 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - determines the absolute value of an integer
+ *
+ *@int: - absolute the true value computed
+ *
+ *Return: absolute value
+ *
+ */
+int _abs(int)
+{
+	int absolute;
+
+	if (num < 0)
+	{
+		absolute = num * -1;
+	}
+	else
+	{
+		absolute = nuum;
+	}
+	return (absolute);
+}
