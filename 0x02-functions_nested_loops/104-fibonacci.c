ee6048ec448b6fd133abf8033a952269a09b856f
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-22 22:37:01 +0000

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num1;
+	long int num2, num3, num4;
+
+	num2 = 0;
+	num3 = 1;
+	for (num1 = 0; num1 < 99; num1++)
+	{
+		num4 = num2 + num3;
+		printf("%ld", num4);
+		if (num1 < 98)
+		{
+			printf(", ");
+		}
+		num2 = num3;
+		num3 = num4;
+	}
+	printf("\n");
+	return (0);
+}
