718ffcf85731dee9da642b52e1a52a64eee09d01
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-22 22:58:52 +0000

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,8 +16,8 @@ int main(void)
 		num4 = num2 + num3;
 		if (num1 == 88)
 		{
-		printf("%d", num4 / 10000000000);
-		printf("%d", num4 % 10000000000);
+		printf("%lu", num4 / 10000000000);
+		printf("%lu", num4 % 10000000000);
 		}
 		else
 		{
