96b4620a7def7896168a5964faee7275799f37e4
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-22 20:49:23 +0000

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,6 +28,12 @@ void print_times_table(int n)
 				_putchar('0' + (product / 10));
 				_putchar('0' + (product % 10));
 			}
+			else
+			{
+				_putchar('0' + (product / 100));
+				_putchar('0' + ((product % 100) / 10));
+				_putchar('0' + ((product % 100) % 10));
+			}
 		}
 		_putchar('\n');
 	}
