9d0957924b873561509c7c02ef221c39135290bb
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 17:57:36 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	if (last-digit < 0)
+	last_digit = num % 10;
+	if (last_digit < 0)
 	{
 		last_digit = last_digit * -1;
 	}
-	last_digit = num % 10;
 	_putchar('0' + last_digit);
 	return (last_digit);
 	_putchar('\n');
