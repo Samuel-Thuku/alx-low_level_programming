57ab4df7e5932289a097d2e2d792263aaae71969
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-21 17:01:29 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign - determines if a number is positive or negative
+* @n: the number being checked
+*
+*Return: 1 and prints + if n is greater than zero
+*Returns 0 and prints 0 if n is zero
+*Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		putchar('+');
+		return (+);
+	}
+}
