e863eaebe324dd3ac146de066a7e64ad5f74b5e7
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-23 19:27:30 +0000

    Task 9

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+*print_triangle - prints a triangle
+*@size: size of triangle
+*
+*/
+void print_triangle(int size)
+{
+	int num1, num2, num3;
+
+	num1 = 0;
+	num2 = size -1;
+	while (num1 < size)
+	{
+		num2 = size - 1 - num1;
+		num3 = num2 + 1;
+		while (num3 > 0)
+		{
+			_putchar(' ');
+			num1--;
+		}
+		while (num3 > 0)
+		{
+			_putchar('#');
+			num3--;
+		}
+		_putchar('\n');
+		num1++;
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
