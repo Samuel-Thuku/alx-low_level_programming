6b20ba132de789dee8ba541b8f20d157af0aff28
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-23 18:43:41 +0000

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,12 +5,12 @@
  */
 void print_square(int size)
 {
+	int num1, num2;
+
 	if (size < 1)
 	{
 		_putchar('\n');
 	}
-	int num1, num2;
-
 	for (num1 = 0; num1 < size; num1++)
 	{
 		for (num2 = 0; num2 < size; num2++)
