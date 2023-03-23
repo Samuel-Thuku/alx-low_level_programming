470460d3677cb6b92f780e723cba79e189dadbf0
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-23 00:17:53 +0000

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,11 +7,12 @@
 int main(void)
 {
 	int num1;
-	unsigned long int num2, num3, num4;
+	unsigned long int num2, num3, num4, num2_1, num2_2, num3_1;
+	unsigned long int num3_2, num4_1, num4_2;
 
 	num2 = 0;
 	num3 = 1;
-	for (num1 = 0; num1 < 93; num1++)
+	for (num1 = 0; num1 < 92; num1++)
 	{
 		num4 = num2 + num3;
 		printf("%lu", num4);
@@ -21,6 +22,24 @@ int main(void)
 		}
 		num2 = num3;
 		num3 = num4;
+	}
+	num2_1 = num2 / 10000000000;
+	num2_2 = num2 % 10000000000;
+	num3_1 = num3 / 10000000000;
+	num3_2 = num3 % 10000000000;
+	for (num1 = 92; num1 < 98; num1++)
+	{
+		num4_1 = num2_1 + num3_1;
+		num4_2 = num2_2 + num3_2;
+		printf("%lu%lu", num4_1, num4_2);
+			if (num1 < 97)
+			{
+				printf(", ");
+			}
+			num2_1 = num3_1;
+			num2_2 = num3_2;
+			num3_1 = num4_1;
+			num3_2 = num4_2;
 	}
 	printf("\n");
 	return (0);
