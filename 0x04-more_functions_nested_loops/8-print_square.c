9d3bb602d8551ef3ee11742df1c55a8bbeb9d52c
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-23 18:36:21 +0000

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_square - prints a square
+ *@size: is the size of the square
+ */
+void print_square(int size)
+{
+	int num1, num2;
+
+	for (num1 = size; num > 0; num--)
+	{
+		for (num2 = size; num > 0; num--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+}
