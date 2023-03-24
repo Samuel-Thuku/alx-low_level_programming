0daa34083e80f8ec872ca2111e8645f0e575b899
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-24 19:39:51 +0000

    Task 12

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <math.h>/**
-*main - Entry point
-*
+#include <math.h>
+/**
+*main - prints the largest prime factor of the number 612852475143
 *Return: Always 0 (Success)
 */
 int main(void)
