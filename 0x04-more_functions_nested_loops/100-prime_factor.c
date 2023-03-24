c0c03dfe98d2f205e7759d46179b231da75ced4d
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-24 19:29:24 +0000

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,5 +6,18 @@
 */
 int main(void)
 {
+	long num1, num2, number;
+	double square;
+
+	number = 612852475143;
+	square = sqrt(number);
+	for (num1 = 1, num1 <= square; num1++)
+	{
+		if (number % x == 0)
+		{
+			num2 = number / num1;
+		}
+	}
+	printf("%ld\n", num2);
 	return (0);
 }
