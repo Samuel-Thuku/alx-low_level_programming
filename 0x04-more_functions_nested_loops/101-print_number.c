58a51d7c62b2f1dffadcae87d76d6b985198d223
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-24 19:47:01 +0000

    Task 13

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,4 +6,21 @@
 */
 void print_number(int n)
 {
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		num = n;
+		_putchar('-');
+	}
+
+	num = num / 10;
+
+	if (num != 0)
+	{
+		print_number(num);
+	}
+
+	_putchar((unsigned int) n % 10 + '0');
 }
