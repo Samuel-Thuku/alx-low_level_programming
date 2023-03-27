230b31746f0edd05337ac4a0dd47c8d676fc0219
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-27 10:38:38 +0000

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*reset_to_98 - a function that updates the value it points to to 98.
+*@n: the numeber being changed.
+*Return: the new value of n.
+*/
+void reset_to_98(int *n)
+{
+	int *num;
+
+	*num = &n;
+	*num = 98;
+	return (*num);
+}
