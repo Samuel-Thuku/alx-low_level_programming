d278b4aefe53d0c7de3819179feffb0787e22bdb
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-27 17:25:42 +0000

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+*rev_string - a function that reverses a string.
+*@s: the string that is being reversed.
+*
+*/
+void rev_string(char *s)
+{
+	int num1, num2;
+
+	num2 = strlen(s);
+	for (num1 = 0; num1 < num2; num1++)
+	{
+		s[num1] = s[(num2 - 1)];
+		num2--;
+	}
+}
