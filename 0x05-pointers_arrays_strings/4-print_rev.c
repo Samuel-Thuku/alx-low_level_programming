c6ff372683de7a287f8d09b4204a7b903978e95d
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-27 17:08:02 +0000

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+*print_rev - a function that prints a string in reverse.
+*@s: the string being printed.
+*
+*/
+void print_rev(char *s)
+{
+	int num1, num2;
+
+	num1 = strlen(s);
+	for (num2 = num1 - 1, num2 >= 0; num2--)
+	{
+		_putchar(s[num2]);
+	}
+	_puchar('\n');
+}
