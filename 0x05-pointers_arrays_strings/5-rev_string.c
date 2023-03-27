47ad6fa9af970a32458ae50c6268e911deb73671
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-27 19:59:27 +0000

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,13 @@
 */
 void rev_string(char *s)
 {
-	int num1, num2;
+	int num1, num2, num3;
 	char letter;
 
+	while (s[num3] != '\0')
+	{
+		num3++;
+	}
 	num1 = strlen(s);
 	for (num2 = 0; num2 <= ((num1 + 2) / 2); num2++)
 	{
