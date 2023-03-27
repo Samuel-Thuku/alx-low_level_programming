2d14f92d1da90980fd6a575ab32978c7caad6e9d
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-27 20:10:28 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+*puts - prints every other character of a string, followed by a new line.
+*@str: the string being displayed.
+*/
+void puts2(char *str)
+{
+	int num1, num2;
+
+	num1 = strlen(str);
+	for (num2 = 0; num2 < num1; num2++)
+	{
+		if ((num2 % 2) = 0)
+		{
+			_putchar(str[num2]);
+		}
+	}
+	_putchar('\n');
+}
