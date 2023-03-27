f22bba6548e10bf4db5e7cd7503be7e9c10acbfe
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-27 11:03:44 +0000

    Task 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_strlen - a function returns the length of a string.
+@s: the string to be measured.
+*Return: the length a string.
+*/
+int _strlen(char *s)
+{
+	strlen(s);
+}
