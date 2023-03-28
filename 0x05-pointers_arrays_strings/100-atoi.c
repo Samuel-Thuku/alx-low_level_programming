0f5317d9435a0e9916b05267d5fcc3de55b0a66d
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-28 07:06:04 +0000

    Task 11

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,16 +4,16 @@
 /**
 *_atoi - a function that converts a string to an integer.
 *@s: the string to be converted.
+*Return: 0 if no integer.
 */
 int _atoi(char *s)
 {
-	int num1, num2, num3;
+	int num1, num2;
 
 	num1 = strlen(s);
-	num3 = 0;
 	for (num2 = 0; num2 < num1; num2++)
 	{
-		num3 = (num3 * 10) + (s[num2] - 48);
+		s[num2] =s[num2] - 48;
 	}
 	return (0);
 }
