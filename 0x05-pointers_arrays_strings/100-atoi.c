bbd12fab9daae93c165b5cafe6d1208197b292e2
Samuel Thuku <samuelngigithuku@gmail.com>
2023-03-28 06:58:12 +0000

    Updated main.h and README.md. Added 100-atoi.c and 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_atoi - a function that converts a string to an integer.
+*@s: the string to be converted.
+*/
+int _atoi(char *s)
+{
+	int num1, num2, num3;
+
+	num1 = strlen(s);
+	num3 = 0;
+	for (num2 = 0; num2 < num1; num2++)
+	{
+		num3 = (num3 * 10) + (s[num2] - 48);
+	}
+	return (0);
+}
